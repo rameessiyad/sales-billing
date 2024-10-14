ff9ee7d606960b19f08b2c5c3410878d1f395457
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-10-14 15:46:05 +0530

    All sales listed

--- client/src/pages/SalesList.jsx
+++ client/src/pages/SalesList.jsx
@@ -21,7 +21,6 @@ const SalesList = () => {
             });
 
             const data = await response.json();
-            console.log(data)
 
             if (response.ok) {
                 setSales(data.sales); 
