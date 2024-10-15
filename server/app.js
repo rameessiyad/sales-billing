ab8b6f4e31437bf533bea747a60822d24bd9c1ab
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-10-15 08:23:33 +0530

    cors origin updated

--- server/app.js
+++ server/app.js
@@ -12,8 +12,13 @@ const app = express();
 
 const PORT = process.env.PORT || 4000;
 
+// Allowed origins
+const allowedOrigins = [
+    'http://localhost:5173'
+];
+
 // Middlewares
-app.use(cors({ credentials: true, origin: 'http://localhost:5173' }));
+app.use(cors({ credentials: true, origin: allowedOrigins }));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
