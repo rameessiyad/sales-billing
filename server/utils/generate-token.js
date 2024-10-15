2bdadf7e786af02d6ed8a813cdce3dd99f191708
rameessiyad <rameesmsiyadofficial786@gmail.com>
2024-10-15 16:35:01 +0530

    cookies storing updated

--- server/utils/generate-token.js
+++ server/utils/generate-token.js
@@ -4,7 +4,6 @@ const generateToken = (res, userId) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '30d' });
     res.cookie('token', token, {
         httpOnly: true,
-        sameSite: 'strict',
         maxAge: 30 * 24 * 60 * 60 * 1000
     })
 }
